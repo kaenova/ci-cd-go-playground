048523c922fbba177a6928cc951e203a5ad5c543
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-07-02 21:49:51 +0700

    Update go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func hello(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte("cannot get hostname\n"))
 		return
 	}
-	w.Write([]byte("hello from version x.x.3 of " + hostname + "\n"))
+	w.Write([]byte("hello from version x.x.4 of docker container " + hostname + "\n"))
 	return
 }
 
